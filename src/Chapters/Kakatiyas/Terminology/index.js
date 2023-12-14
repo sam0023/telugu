90d6c66fa890483022af841eb28e9773178b195f
sameer <sdsameer61116@gmail.com>
2023-12-14 11:32:17 +0530

    a

--- src/Chapters/Kakatiyas/Terminology/index.js
+++ src/Chapters/Kakatiyas/Terminology/index.js
@@ -12,95 +12,274 @@ const Terminology = [
 		ans: ['Tax payed by peasent to king as a GIFT'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'పన్నాస',
+		ans: ['దాన భూముల మీద రాయితీ పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'దరిశనము',
+		ans: ['రాజు దర్శనార్థం కొరకు చెల్లించే పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'నీరువిడి',
+		ans: ['రాట్నాల ద్వారా నీరు తోడుకున్నందుకు విధించే పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'పర',
+		ans: ['మాగాణి భూములపై విధించు పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'అంతరాయము',
+		ans: ['పోక తోటల మీద పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'కిలారము',
+		ans: ['ఆవుల, గొర్రెల మీద ప్రత్యేక పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'వెన్నుపన్ను',
+		ans: ['పంట కోతకు వచ్చిన సమయంలో విధించే పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'ఒంటెల పన్ను',
+		ans: ['నాయంకర గ్రామాల్లోని రైతుల మీద విధించు పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'పంగం',
+		ans: ['మెట్ట భూములపై విధించు పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'పుల్లరి',
+		ans: ['పచ్చికబీడులపై పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'పంగం',
+		ans: ['ధన రూపంలో చెల్లించు పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'మధ్యకం',
+		ans: ['ఉమ్మడిగా గ్రామస్తుల మీద విధించే అత్యవసర పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'గణాచారి పన్ను',
+		ans: ['వేశ్యల నుండి, బిక్షగాళ్ళ నుండి వసూలు చేయు పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'గాండీ సుంకం',
+		ans: ['మోటబావి మీద వేసే పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'పుట్టు పేరు సుంకం',
+		ans: ['పుట్టిన బిడ్డకు నామకరణం చేసే సందర్భంగా వేసే పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'ఆలిము',
+		ans: ['కూరగాయల మీద పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'మాగాణి',
+		ans: ['మాగాణి అనగా సారవంతమైన పుష్కలంగా నీటి సౌకర్యం గల వ్యవసాయ భూమి'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'సుంకం',
+		ans: ['వాణిజ్య వస్తువులపై విధించే పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'తీర్పరి',
+		ans: ['అంగడిలో అమ్మకం, కొనుగోలు వస్తువుల విలువలను లెక్కగట్టేవాడు.'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'సుంకమాన్యగాడు',
+		ans: ['అంగడిలో సుంకం వసూలు చేసేవాడు.'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'అలము',
+		ans: ['కూరగాయల మీద పన్ను'],
 	},
 	{
-		questionText: '',
-		ans: [''],
+		questionText: 'కర పట్టణాలు',
+		ans: ['ఎగుమతి, దిగుమతి సుంకాలను వసూలు చేసే రేవు పట్టణాలు.'],
 	},
 	{
-		questionText: '',
+		questionText: 'కాలగాడు',
+		ans: ['సరకుల్ని కొలిచేవారు'],
+	},
+	{
+		questionText: 'పెరిక ఎడ్ల సుంకం',
+		ans: ['సంచులను మోసే ఎడ్ల బండ్లపై విధించు సుంకం'],
+	},
+	{
+		questionText: 'ఉప్పు పెరిక సుంకం',
+		ans: ['ఉప్పు బస్తాలపై పన్ను.'],
+	},
+	{
+		questionText: 'సుంకరి',
+		ans: ['ఒక అడ్డలో సుంకం వసూలు హక్కుల్ని కొనుక్కునేవారు'],
+	},
+	{
+		questionText: 'ఫలదారు',
+		ans: ['సుంక నిర్ధారణ కొరకు వ్యవసాయోత్పత్తి విలువను అంచనా వేసేవాడు'],
+	},
+	{
+		questionText: 'అమ్ముబడి సుంకం',
+		ans: ['అమ్మిన వస్తువులపై వేసే సుంకం'],
+	},
+
+	// Taxes
+
+	{
+		questionText: 'నియోగులు',
+		ans: ['Head of Nadulu'],
+	},
+	{
+		questionText: 'పారుపత్యంగార్లు',
+		ans: ['Head of స్థలలు'],
+	},
+	{
+		questionText: 'ఆయగార్లు',
+		ans: ['Head of Village'],
+	},
+	{
+		questionText: 'Reddy',
+		ans: ['Tax collector', 'న్యాయాధికారి'],
+	},
+	{
+		questionText: 'కరణం',
+		ans: ['Land Registar'],
+	},
+	{
+		questionText: 'తలారి',
+		ans: ['Police'],
+	},
+	{
+		questionText: 'నీరుకట్టు',
+		ans: ['One who supplies lake water to farmers'],
+	},
+	{
+		questionText: 'ప్రాద్వివాక్యులు',
+		ans: ['One who helps king in Judicial decision making'],
+	},
+	{
+		questionText: 'మహాజనులు',
+		ans: ['స్థానిక న్యాయాధికారులు'],
+	},
+	{
+		questionText: 'సముద్రిత',
+		ans: ['courts under ప్రాద్వివాక్యులు'],
+	},
+	{
+		questionText: 'ప్రతిష్ఠిత',
+		ans: ['Temparay courts in central Administration'],
+	},
+	{
+		questionText: 'అప్రతిష్ఠిత',
+		ans: ['Temparay courts in central Villages'],
+	},
+	{
+		questionText: 'శాశ్వత',
+		ans: ['courts in which పాలకుడు himself acts as Judge'],
+	},
+	{
+		questionText: 'లెంకలు',
+		ans: ['Boudygaurds of Kings'],
+	},
+	{
+		questionText: 'పోడు',
+		ans: ['కొత్తగా సాగు చేసిన భూమి'],
+	},
+	{
+		questionText: 'రాచపొలం',
+		ans: ['రాజుపొలం'],
+	},
+	{
+		questionText: 'రాచదొడ్డి',
+		ans: ['రాజుపొలం'],
+	},
+	{
+		questionText: 'అరి',
+		ans: ['Land Tax'],
+	},
+	{
+		questionText: 'అరిగాపులు',
+		ans: ['One who pays land tax'],
+	},
+	{
+		questionText: 'ఆయగండ్రు',
+		ans: ['One who collects tax'],
+	},
+	{
+		questionText: 'కేసరి పాటిగడ',
+		ans: ['Land Measuring Instrument'],
+	},
+	{
+		questionText: 'అర్థాయం',
+		ans: ['రాచపొలం కౌలుకి తీసుకోవడం'],
+	},
+	{
+		questionText: 'అర్ధసిరి',
+		ans: ['రాచపొలం కౌలుకి తీసుకోవడం'],
+	},
+	{
+		questionText: 'గద్యానం',
+		ans: ['Gold coin'],
+	},
+	{
+		questionText: 'మాడ',
+		ans: ['Silver coin'],
+	},
+	{
+		questionText: 'రుక',
+		ans: ['Silver coin'],
+	},
+	{
+		questionText: '1 గద్యానం = _____ రుకలు',
+		ans: ['10'],
+	},
+	{
+		questionText: '1 రుక = _____ వీసాలు',
+		ans: ['16'],
+	},
+	{
+		questionText: '1 గద్యానం = _____ వీసాలు',
+		ans: ['160'],
+	},
+	{
+		questionText: 'కుంభాకార',
+		ans: ['కుమ్మరి'],
+	},
+	{
+		questionText: 'అయస్కార',
+		ans: ['కమ్మరి'],
+	},
+	{
+		questionText: 'సువర్ణకార',
+		ans: ['కంసాలి'],
+	},
+	{
+		questionText: 'తక్షక',
+		ans: ['వడ్రంగి'],
+	},
+	{
+		questionText: 'నాపిత',
+		ans: ['మంగలి'],
+	},
+	{
+		questionText: 'రజక',
+		ans: ['చాకలి'],
+	},
+	{
+		questionText: 'మడి సంతలు',
+		ans: ['సంతలు form plp inside the fort'],
+	},
+	{
+		questionText: 'మైల సంతలు',
+		ans: ['సంతలు form plp outside the fort (lower caste)'],
+	},
+	{
+		questionText: 'పద్మనాయకులు',
+		ans: ['Sect from Vishnavism'],
+	},
+	{
+		questionText: 'మదదారులు',
 		ans: [''],
 	},
 	{
